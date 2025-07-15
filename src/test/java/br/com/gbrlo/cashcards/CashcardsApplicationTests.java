987f7b46f9db09611cfc60374d700a23d3dcae91
Gabriel Oliveira <br.gabrieldeoliveira@gmail.com>
2025-07-15 20:54:10 -0300

    Project created

---
+++ src/test/java/br/com/gbrlo/cashcards/CashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gbrlo.cashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
