87f83776f9c3e619e0cc3035aed0777372946b53
Gabriel Oliveira <br.gabrieldeoliveira@gmail.com>
2025-07-15 21:22:27 -0300

    First test

---
+++ src/test/java/br/com/gbrlo/cashcards/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package br.com.gbrlo.cashcards;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class CashCardJsonTest {
+
+    @Test
+    void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+}
