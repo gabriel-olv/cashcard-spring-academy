82a2e7ec698fab2d67c7505dee6089d94bdb3dce
Gabriel Oliveira <br.gabrieldeoliveira@gmail.com>
2025-07-15 22:35:55 -0300

    Implementing GET: find by id

--- src/test/java/br/com/gbrlo/cashcards/CashcardsApplicationTests.java
+++ src/test/java/br/com/gbrlo/cashcards/CashcardsApplicationTests.java
@@ -1,13 +1,36 @@
 package br.com.gbrlo.cashcards;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardsApplicationTests {
 
+	@Autowired
+	TestRestTemplate rest;
+
 	@Test
-	void contextLoads() {
+	void shouldReturnACashCarshWhenDataIsSaved() {
+		var response = rest.getForEntity("/cashcards/99", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		var documentContext = JsonPath.parse(response.getBody());
+		var id = documentContext.read("$.id");
+		var amount = documentContext.read("$.amount");
+		assertThat(id).isEqualTo(99);
+		assertThat(amount).isEqualTo(123.45);
 	}
 
+	@Test
+	void shouldNotReturnACashCardWithUnknownId() {
+		var response = rest.getForEntity("/cashcards/1000", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
 }
