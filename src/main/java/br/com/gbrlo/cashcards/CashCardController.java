e06acb9370a9e41fcd191e4419e9236ce95758aa
Gabriel Oliveira <br.gabrieldeoliveira@gmail.com>
2025-07-15 22:52:59 -0300

    Repository and Spring Data

--- src/main/java/br/com/gbrlo/cashcards/CashCardController.java
+++ src/main/java/br/com/gbrlo/cashcards/CashCardController.java
@@ -1,20 +1,29 @@
 package br.com.gbrlo.cashcards;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    private final CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if (requestedId.equals(99L)) {
-            var cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+        var cashCardOptional = cashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         }
         return ResponseEntity.notFound().build();
     }
---
+++ src/main/java/br/com/gbrlo/cashcards/CashCardRepository.java
@@ -0,0 +1,6 @@
+package br.com.gbrlo.cashcards;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+}
